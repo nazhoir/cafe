f5aee59993e5be1025cf77c62529a58469652da0
nazhoir <nazhoirulamin031201@gmail.com>
2022-07-01 23:43:07 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'primary' : '#834D1E',
+        'secondary' : '#F8E3B6',
+        'aksen-1' : '#FCF2D9',
+      },
+    },
+  },
+  plugins: [],
+}
